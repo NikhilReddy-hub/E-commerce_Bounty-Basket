dc14c7b185019364c57dd395742727abe783fbab
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2025-01-11 15:10:16 +0530

    Add files via upload

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Ensure this file includes Tailwind styles
+import Login from "./Login";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+);
