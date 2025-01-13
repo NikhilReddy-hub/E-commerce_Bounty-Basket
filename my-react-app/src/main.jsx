f4260e7f6849161841314eaacd751cc14cb9b009
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2025-01-13 16:56:47 +0530

    milestone-2

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Ensure this file includes Tailwind styles
-import Login from "./Login";
+import Login from "./login";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- my-react-app/tailwind.config.js
+++ my-react-app/tailwind.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#4f46e5", // Custom primary color
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
