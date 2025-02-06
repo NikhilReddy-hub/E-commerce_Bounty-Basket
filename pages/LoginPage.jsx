11e97ac122fbd9355d211cbcc77cfcbd13c88b02
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2025-02-06 12:32:57 +0530

    Add files via upload

--- pages/LoginPage.jsx
+++ pages/LoginPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Login from '../components/auth/login'
-
-const LoginPage = () => {
+import Login from "../components/auth/Login"
+function LoginPage() {
   return (
     <div>
-      <Login />
+      <Login/>
     </div>
   )
 }
 
-export default LoginPage
+export default LoginPage;
