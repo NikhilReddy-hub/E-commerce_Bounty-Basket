ae5c66e49471fb871b21c940b6d1868ec7b09aba
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2025-02-08 14:46:00 +0530

    Delete my-react-app directory

--- my-react-app/src/routes.jsx
+++
@@ -1,6 +0,0 @@
-import LoginPage from "../pages/LoginPage.jsx";
-import Login from "../components/Auth/Login.jsx";
-import Signup from "../components/Auth/Signup.jsx"
-import SignupPage from "../pages/SignupPage.jsx";
-
-export {Login,LoginPage,Signup,SignupPage};
